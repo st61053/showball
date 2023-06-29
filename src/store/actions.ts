9758ba3710700e05819c27564e6b123022213d77
Matěj Varga <varga.matej@seznam.cz>
2023-06-29 20:41:48 +0200

    frontend 1.0.0

--- src/store/actions.ts
+++
@@ -1,7 +0,0 @@
-import { IToken } from "../tokens/types";
-import { CHANGE_SELECTED_TOKEN } from "./constants";
-
-export const changeSelectedToken = (token: IToken) => ({
-    token,
-    type: CHANGE_SELECTED_TOKEN
-  });
--- src/store/types.ts
+++
@@ -1,5 +0,0 @@
-import { IToken } from "../tokens/types";
-
-export interface IStoreState {
-    selectedToken: IToken |null;
-}
